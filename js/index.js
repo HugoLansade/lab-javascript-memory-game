c718645362369e3bbf28680ad6b640d02eb6feec
Hugo Lansade <lhugo@lansade.com>
2021-10-01 09:03:07 +0200

    done need to add an alert for end game

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//import { MemoryGame } from './memory.js';
+
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -25,6 +27,9 @@ const cards = [
   { name: 'thor', img: 'thor.jpg' }
 ];
 
+const pairsClicked = document.getElementById('pairs-clicked');
+const pairsGuessed = document.getElementById('pairs-guessed');
+
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
@@ -41,11 +46,55 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+   function cardCliked(card){
+    card.classList.add("turned");
+   }
+   function cardReset(card){
+     card.classList.remove("turned");
+   }
+
+
   // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
+  document.querySelectorAll('.card').forEach((card) => {    
     card.addEventListener('click', () => {
+      memoryGame.pickedCards.push(card);                                // on ajoute la carte à notre arret
+      console.log("nbout= "+memoryGame.pairsClicked);
+      //if(memoryGame.pickedCards.length === 3) clearTimeout(intervalId);
+      if(memoryGame.pickedCards.length < 2){                             //si on a que une carte
+        
+        cardCliked(card);                                             // on la montre
+        
+        //console.log("nbin1= "+memoryGame.pairsClicked);
+        //console.log("***dans le if")        
+      } else if (memoryGame.pickedCards.length === 2){              //on a retourné deux cartes on empeche le mec de cliquer 3fois ou plus
+        cardCliked(card);        
+        
+                                                                    //verification de si les cartes sont similaires
+        if(memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])){
+          
+          memoryGame.pairsGuessed ++;
+          pairsGuessed.textContent = `${memoryGame.pairsGuessed/2}`;
+          memoryGame.pickedCards = [];                             //On réinitialise le tab
+          //console.log("nbin2.1= "+memoryGame.pairsClicked);
+                                                                  //on ecrit dans le html le nombre de pair guessed
+        } else {// les cartes ne sont pas les m^mes on les retournes apres un délai
+          //console.log("***dans le else2") 
+          //console.log("nbin2.2= "+ memoryGame.pairsClicked);
+          let intervalId = setTimeout(() => {            
+            cardReset(memoryGame.pickedCards[0]);                   // on enleve la propriété turned
+            cardReset(memoryGame.pickedCards[1]);                   // on enleve la propriété turned
+            memoryGame.pickedCards = [];                            //vide le tableau          
+                                                                    //on retourne les cartes
+          },3000);
+        }
+        pairsClicked.textContent = `${memoryGame.pairsClicked}`;
+
+      }
+
+      console.log("dans la fonction")
       // TODO: write some code here
       console.log(`Card clicked: ${card}`);
     });
   });
+  console.log("hors la fonction")
 });
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,50 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    let finalArrayShuffled = [];
+    let buffer = this.cards;
+    if(this.cards.length === 0) return undefined;
+
+    console.log(">>>>>>>>>>before boucle="+ buffer.length);
+    for (let index = 0; index <= buffer.length; index++) { // <=buffer.length
+      let newMixedPosition = Math.floor(Math.random() * buffer.length);
+
+      finalArrayShuffled[newMixedPosition] = buffer[newMixedPosition];
+
+      buffer.splice(newMixedPosition,1);
+
+      console.log(">>>>>>>>>>after boucle="+ buffer.length);
+    }
+        
+    console.log("fin boucle>>>>>>>>>>"+ buffer.length);
+    console.log(">>>>>>>>>>TailleFinale"+finalArrayShuffled.length);
+    return finalArrayShuffled;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked ++;
+    if(card1.getAttribute('data-card-name') === card2.getAttribute('data-card-name')){
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    //return (this.pairsGuessed === this.cards.length/2); //return soit true soit false
+    //Autre methode
+    if(this.pairsGuessed === this.cards.length/2){
+      return true;
+    } else {
+      return false;
+    }    
   }
 }
 
